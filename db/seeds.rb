6a2d0f3c1019940ebee4e6c2d6ed83b1d73e4b06
kinta0402 <goro0410@gmail.com>
2019-08-23 03:25:02 +0000

    上長1追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,14 @@ User.create!(name:  "管理者",
              email: "email@sample.com",
              password:              "password",
              password_confirmation: "password",
-             admin: true)
+             admin: true,
+             superior: true)
+
+User.create!(name: "上長1",
+             email: "superior1@sample.com",
+             password:              "password",
+             password_confirmation: "password",
+             superior: true)
 
 59.times do |n|
   name  = Faker::Name.name
