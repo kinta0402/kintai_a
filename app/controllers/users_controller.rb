0054b19f00c1e5dc3b5a959710b9b05986bf5d02
kinta0402 <goro0410@gmail.com>
2019-09-02 07:27:06 +0000

    管理者ヘッダーview編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   
   before_action :correct_user,   only: [:edit, :update]
   
-  before_action:admin_user,      only: [:destroy, :edit_basic_info, :update_basic_info, :index]
+  before_action:admin_user,      only: [:destroy, :edit_basic_infodestroy, :edit_basic_info, :update_basic_info, :index]
   
   def index # 全てのユーザーを表示する
     @users = User.paginate(page: params[:page]).search(params[:search])
