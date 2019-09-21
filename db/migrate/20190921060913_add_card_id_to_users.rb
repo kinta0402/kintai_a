75d36e1c79fbfdabadf43500a508fc08da88fb74
kinta0402 <goro0410@gmail.com>
2019-09-21 06:54:18 +0000

    カードid追加

--- db/migrate/20190921060913_add_card_id_to_users.rb
+++ db/migrate/20190921060913_add_card_id_to_users.rb
@@ -1,5 +1,5 @@
 class AddCardIdToUsers < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :department, :string
+    add_column :users, :card_id, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190823030420) do
+ActiveRecord::Schema.define(version: 20190921060913) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -31,10 +31,11 @@ ActiveRecord::Schema.define(version: 20190823030420) do
     t.string "password_digest"
     t.string "department"
     t.boolean "admin", default: false
-    t.datetime "basic_time", default: "2019-02-19 22:30:00" #指定勤務時間
-    t.datetime "work_time", default: "2019-02-19 23:00:00" #基本勤務時間
+    t.datetime "basic_time", default: "2019-02-19 22:30:00"
+    t.datetime "work_time", default: "2019-02-19 23:00:00"
     t.string "remember_digest"
     t.boolean "superior", default: false
+    t.string "card_id"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
